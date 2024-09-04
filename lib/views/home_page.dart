3dafb64c1993962da576d2ad0f9c7b0e1b403300
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-09-04 23:34:40 +0300

    create Drawer and add UserAccountsDrawerHeader

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,26 +7,68 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/image/nature2.jpg"),
+                  fit: BoxFit.cover),
+    ),
+                  currentAccountPicture: CircleAvatar(
+                    radius: 55,
+                    backgroundImage: AssetImage("assets/image/ghada.jpg"),
+                  ),
+                  accountName: Text(
+                    "Ghada Mohamed ",
+                    style: TextStyle(),
+                  ),
+                  accountEmail: Text("ghada@gmail.com"),),
+    ],
+            ),
+
+
+        ),
+
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
-        title: Text("Home",style: TextStyle( color: Colors.white),),
-        leading: Icon(Icons.menu,color: Colors.white),
+        title: Text(
+          "Home",
+          style: TextStyle(color: Colors.white),
+        ),
         actions: [
           Row(
             children: [
-              IconButton(onPressed: (){}, icon: Icon(Icons.add_shopping_cart,color: Colors.white,),),
+              Stack(children: [
+                Container(
+                  child: Text(
+                    "8",
+                    style: TextStyle(fontSize: 10),
+                  ),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.greenAccent),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.white,
+                  ),
+                ),
+              ]),
               Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text("\$13",style: TextStyle(fontSize: 19,color: Colors.white),),
+                padding: const EdgeInsets.only(right: 12),
+                child: Text(
+                  "\$13",
+                  style: TextStyle(fontSize: 19, color: Colors.white),
+                ),
               ),
-
-
             ],
           )
-
         ],
       ),
-
     );
   }
 }
